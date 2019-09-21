a72e927985c768adbb47ed52378f76c785aa278b
Nikita Politaev <nipolit@gmail.com>
2019-09-21 16:51:44 +0700

    Remove unnecessary CharCounterService

--- src/main/java/com/politaev/whiterabbit/counter/CharCounterService.java
+++
@@ -1,7 +0,0 @@
-package com.politaev.whiterabbit.counter;
-
-public class CharCounterService {
-    public CharCount countChars(String string) {
-        return CharCount.countLatinChars(string);
-    }
-}
--- src/test/java/com/politaev/whiterabbit/counter/CharCountTest.java
+++ src/test/java/com/politaev/whiterabbit/counter/CharCountTest.java
@@ -1,25 +1,18 @@
 package com.politaev.whiterabbit.counter;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import static com.politaev.whiterabbit.counter.CharCount.countLatinChars;
 import static org.junit.Assert.assertEquals;
 
 public class CharCountTest {
-    CharCounterService charCounterService;
-
-    @Before
-    public void initTest() {
-        charCounterService = new CharCounterService();
-    }
-
     @Test
     public void testCharCountSum() {
         String string1 = "benedict", string2 = "cumberbatch";
-        CharCount charCount1 = charCounterService.countChars(string1);
-        CharCount charCount2 = charCounterService.countChars(string2);
+        CharCount charCount1 = countLatinChars(string1);
+        CharCount charCount2 = countLatinChars(string2);
         CharCount actualSum = charCount1.add(charCount2);
-        CharCount expectedSum = charCounterService.countChars(string1 + string2);
+        CharCount expectedSum = countLatinChars(string1 + string2);
         assertEquals(expectedSum, actualSum);
     }
 }
