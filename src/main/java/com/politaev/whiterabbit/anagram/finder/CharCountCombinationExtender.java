a1a7dafa35a22af377ecb05a5011ce92dd185250
Nikita Politaev <nipolit@gmail.com>
2019-10-19 16:06:38 +0700

    Implement class appending CharCount combination according to conditions

--- src/main/java/com/politaev/whiterabbit/anagram/finder/CharCountCombinationExtender.java
+++ src/main/java/com/politaev/whiterabbit/anagram/finder/CharCountCombinationExtender.java
@@ -1,8 +1,13 @@
 package com.politaev.whiterabbit.anagram.finder;
 
+import com.politaev.whiterabbit.combinatorics.Combination;
 import com.politaev.whiterabbit.counter.CharCount;
 import com.politaev.whiterabbit.dictionary.CharCountTaxonomy;
 import com.politaev.whiterabbit.dictionary.Dictionary;
+
+import java.util.stream.Stream;
+
+import static com.politaev.whiterabbit.anagram.finder.CharCountCombination.wrap;
 
 class CharCountCombinationExtender {
     private final CharCountTaxonomy charCountTaxonomy;
@@ -22,5 +27,106 @@ class CharCountCombinationExtender {
         this.totalCharsThreshold = totalCharsThreshold;
         this.totalCharsLimit = totalCharsLimit;
         this.resultCharCountLimit = resultCharCountLimit;
+    }
+
+    Stream<Combination<CharCount>> extend(Combination<CharCount> originalCombination) {
+        CharCountCombination charCountCombination = wrap(originalCombination);
+        requireCombinationCompatibleWithBoundaries(charCountCombination);
+        AdditionFinder additionFinder = new AdditionFinder(charCountCombination);
+        return additionFinder.findAdditions()
+                .map(charCountCombination::add);
+    }
+
+    private void requireCombinationCompatibleWithBoundaries(CharCountCombination combination) {
+        requireCombinationCompatibleWithUpperBoundary(combination);
+        requireCombinationCompatibleWithLowerBoundary(combination);
+    }
+
+    private void requireCombinationCompatibleWithUpperBoundary(CharCountCombination combination) {
+        if (resultCharCountLimit != null) {
+            requireCombinationIncludedInCharCountLimit(combination);
+        }
+        if (totalCharsLimit != null) {
+            requireTotalCharsLowerThanLimit(combination);
+        }
+    }
+
+    private void requireCombinationIncludedInCharCountLimit(CharCountCombination combination) {
+        if (!resultCharCountLimit.includes(combination.getCharCountSum())) {
+            throw new IllegalArgumentException("Combination not included in CharCount limit");
+        }
+    }
+
+    private void requireTotalCharsLowerThanLimit(CharCountCombination combination) {
+        if (combination.getTotalChars() >= totalCharsLimit) {
+            throw new IllegalArgumentException("Combination total chars not lower than limit");
+        }
+    }
+
+    private void requireCombinationCompatibleWithLowerBoundary(CharCountCombination combination) {
+        if (totalCharsThreshold != null) {
+            requireTotalCharsNotHigherThanThreshold(combination);
+        }
+    }
+
+    private void requireTotalCharsNotHigherThanThreshold(CharCountCombination combination) {
+        if (combination.getTotalChars() > totalCharsThreshold) {
+            throw new IllegalArgumentException("Combination total chars higher than threshold");
+        }
+    }
+
+    private class AdditionFinder {
+        private final CharCountCombination originalCombination;
+        private final int additionTotalCharsThreshold;
+        private final int additionTotalCharsLimit;
+        private final CharCount additionCharCountLimit;
+
+        private AdditionFinder(CharCountCombination originalCombination) {
+            this.originalCombination = originalCombination;
+            this.additionTotalCharsThreshold = determineAdditionTotalCharsThreshold();
+            this.additionTotalCharsLimit = determineAdditionTotalCharsLimit();
+            this.additionCharCountLimit = determineAdditionCharCountLimit();
+        }
+
+        private int determineAdditionTotalCharsThreshold() {
+            return (totalCharsThreshold != null) ? totalCharsThreshold - originalCombination.getTotalChars() : 0;
+        }
+
+        private int determineAdditionTotalCharsLimit() {
+            return (totalCharsLimit != null) ? totalCharsLimit - originalCombination.getTotalChars() : Integer.MAX_VALUE;
+        }
+
+        private CharCount determineAdditionCharCountLimit() {
+            return (resultCharCountLimit != null) ? resultCharCountLimit.subtract(originalCombination.getCharCountSum()) : null;
+        }
+
+        Stream<CharCount> findAdditions() {
+            Stream<CharCount> additionsWithinTotalCharsBoundaries = findAdditionsWithinTotalCharsBoundaries();
+            if (additionCharCountLimit != null) {
+                return filterAdditionsIncludedInCharCountLimit(additionsWithinTotalCharsBoundaries);
+            } else {
+                return additionsWithinTotalCharsBoundaries;
+            }
+        }
+
+        Stream<CharCount> findAdditionsWithinTotalCharsBoundaries() {
+            if (isThresholdEffectiveLowerBoundary()) {
+                return charCountTaxonomy.charCountsFromTotalCharsToTotalChars(additionTotalCharsThreshold, additionTotalCharsLimit);
+            } else {
+                return charCountTaxonomy.charCountsFromElementToTotalChars(getCurrentLast(), additionTotalCharsLimit);
+            }
+        }
+
+        private boolean isThresholdEffectiveLowerBoundary() {
+            return getCurrentLast().totalChars() < additionTotalCharsThreshold;
+        }
+
+        private CharCount getCurrentLast() {
+            return originalCombination.get(originalCombination.size() - 1);
+        }
+
+        private Stream<CharCount> filterAdditionsIncludedInCharCountLimit(Stream<CharCount> additions) {
+            return additions.filter(additionCharCountLimit::includes);
+        }
     }
 }
---
+++ src/test/java/com/politaev/whiterabbit/anagram/finder/CharCountCombinationExtenderTest.java
@@ -0,0 +1,159 @@
+package com.politaev.whiterabbit.anagram.finder;
+
+import com.politaev.whiterabbit.anagram.AnagramTest;
+import com.politaev.whiterabbit.combinatorics.Combination;
+import com.politaev.whiterabbit.counter.CharCount;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.politaev.whiterabbit.anagram.finder.CharCountCombinationExtender.createExtender;
+import static org.fest.assertions.Assertions.assertThat;
+
+
+public class CharCountCombinationExtenderTest extends AnagramTest {
+
+    @Test
+    public void testNoRestrictions() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("aa", "bb", "aaab");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("aa", "bb", "aaab", "aaab"),
+                charCountCombinationOf("aa", "bb", "aaab", "aabb"),
+                charCountCombinationOf("aa", "bb", "aaab", "abbb"),
+                charCountCombinationOf("aa", "bb", "aaab", "bbbb")
+        );
+    }
+
+    @Test
+    public void testTotalCharsLimit() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultTotalCharsNotHigherThan(6)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("a", "bb");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("a", "bb", "bb"),
+                charCountCombinationOf("a", "bb", "aaa"),
+                charCountCombinationOf("a", "bb", "aab"),
+                charCountCombinationOf("a", "bb", "abb"),
+                charCountCombinationOf("a", "bb", "bbb")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationNotUnderTotalCharsLimitIllegal() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultTotalCharsNotHigherThan(3)
+                .build();
+        combinationExtender.extend(charCountCombinationOf("a", "bb"));
+    }
+
+    @Test
+    public void testTotalCharsThreshold() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultTotalCharsNotLowerThan(4)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("a");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("a", "aaa"),
+                charCountCombinationOf("a", "aab"),
+                charCountCombinationOf("a", "abb"),
+                charCountCombinationOf("a", "bbb"),
+                charCountCombinationOf("a", "aaaa"),
+                charCountCombinationOf("a", "aaab"),
+                charCountCombinationOf("a", "aabb"),
+                charCountCombinationOf("a", "abbb"),
+                charCountCombinationOf("a", "bbbb")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationNotUnderExpectedTotalCharsIllegal() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultTotalCharsNotLowerThan(2)
+                .build();
+        combinationExtender.extend(charCountCombinationOf("a", "bb"));
+    }
+
+    @Test
+    public void testTotalCharsExactValue() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withExpectedTotalChars(4)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("a");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("a", "aaa"),
+                charCountCombinationOf("a", "aab"),
+                charCountCombinationOf("a", "abb"),
+                charCountCombinationOf("a", "bbb")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationOverTotalCharsThresholdIllegal() {
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withExpectedTotalChars(3)
+                .build();
+        combinationExtender.extend(charCountCombinationOf("a", "bb"));
+    }
+
+    @Test
+    public void testResultCharCountLimit() {
+        CharCount resultCharCountLimit = charCounter.countChars("b" + "ab" + "abbbb");
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultCharCountIncludedIn(resultCharCountLimit)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("b", "ab");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("b", "ab", "ab"),
+                charCountCombinationOf("b", "ab", "bb"),
+                charCountCombinationOf("b", "ab", "abb"),
+                charCountCombinationOf("b", "ab", "bbb"),
+                charCountCombinationOf("b", "ab", "abbb"),
+                charCountCombinationOf("b", "ab", "bbbb")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationEqualToResultCharCountLimitIllegal() {
+        CharCount resultCharCountLimit = charCounter.countChars("b" + "ab");
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultCharCountIncludedIn(resultCharCountLimit)
+                .build();
+        combinationExtender.extend(charCountCombinationOf("b", "ab"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationNotIncludedInResultCharCountLimitIllegal() {
+        CharCount resultCharCountLimit = charCounter.countChars("b" + "ab");
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultCharCountIncludedIn(resultCharCountLimit)
+                .build();
+        combinationExtender.extend(charCountCombinationOf("a", "a"));
+    }
+
+    @Test
+    public void testAllConditionsAtOnce() {
+        CharCount resultCharCountLimit = charCounter.countChars("b" + "abbbb");
+        CharCountCombinationExtender combinationExtender = createExtender(dictionary)
+                .withResultCharCountIncludedIn(resultCharCountLimit)
+                .withResultTotalCharsNotLowerThan(3)
+                .withResultTotalCharsNotHigherThan(4)
+                .build();
+        Combination<CharCount> initialCombination = charCountCombinationOf("b");
+        List<Combination<CharCount>> extendedCombinations = combinationExtender.extend(initialCombination).collect(Collectors.toList());
+        assertThat(extendedCombinations).containsExactly(
+                charCountCombinationOf("b", "ab"),
+                charCountCombinationOf("b", "bb"),
+                charCountCombinationOf("b", "abb"),
+                charCountCombinationOf("b", "bbb")
+        );
+    }
+}
