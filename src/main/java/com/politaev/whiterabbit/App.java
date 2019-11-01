256bb9172733ef17aae34043b71700d4f1b446a1
Nikita Politaev <nipolit@gmail.com>
2019-11-01 18:51:18 +0700

    Start from the command line

---
+++ src/main/java/com/politaev/whiterabbit/App.java
@@ -0,0 +1,58 @@
+package com.politaev.whiterabbit;
+
+import com.politaev.whiterabbit.controller.ApplicationController;
+import picocli.CommandLine;
+
+import static com.politaev.whiterabbit.controller.ApplicationController.initializeApplicationContext;
+
+@CommandLine.Command(
+        name = "white-rabbit",
+        description = "Searches anagrams of the given phrase having specified md5 sums.",
+        version = {"white-rabbit 1.0", "@|green Nikita Politaev (c) 2019|@"},
+        sortOptions = false,
+        mixinStandardHelpOptions = true,
+        headerHeading = "@|bold,underline Usage|@:%n%n",
+        descriptionHeading = "%n@|bold,underline Description|@:%n%n",
+        parameterListHeading = "%n@|bold,underline Parameters|@:%n",
+        optionListHeading = "%n@|bold,underline Options|@:%n",
+        customSynopsis = "white-rabbit [-hV] -p=PHRASE -l=SIZE MD5...")
+public class App implements Runnable {
+
+    @CommandLine.Option(
+            names = {"-p", "--phrase"},
+            required = true,
+            description = "Search for anagrams of this phrase.",
+            paramLabel = "PHRASE")
+    private String givenPhrase;
+
+    @CommandLine.Option(
+            names = {"-l", "--word-limit"},
+            required = true,
+            description = "Consider anagrams with at most this number of words.",
+            paramLabel = "SIZE")
+    private int wordLimit;
+
+    @CommandLine.Parameters(
+            arity = "1..*",
+            description = "Md5 sums to search for.",
+            paramLabel = "MD5")
+    private String[] md5SumsToFind;
+
+    public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public void run() {
+        try {
+            ApplicationController applicationController = initializeApplicationContext()
+                    .withGivenPhrase(givenPhrase)
+                    .withWordLimit(wordLimit)
+                    .withMd5SumsToFind(md5SumsToFind)
+                    .createController();
+            applicationController.runApplication();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
